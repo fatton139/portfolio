b8ef1f105f6c906bb4f61e594fbc6ffdd71e3a6b
aXises <leletonyli.13944@yahoo.com.au>
2016-12-29 20:12:55 +1100

    Navigation
    Included ionicons fonts files

--- script/script.js
+++ script/script.js
@@ -2,6 +2,19 @@
 $(function() {
     "use strict";	
 	$(document).ready(function() { 
+	
+		$("header").interactive_bg({
+			strength: 10,              
+			scale: 1.01,               
+			animationSpeed: "100ms"
+		});
+		
+		$(window).resize(function() {
+			$("header > .ibg-bg").css({
+				width: $(window).outerWidth(),
+				height: $(window).outerHeight()
+			});
+		});
 
 	}); //end of document ready
 }); //end of use
